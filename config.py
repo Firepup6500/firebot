9402a536f70cc32371a301be2d0fe9d7bf9423b0
Firepup Sixfifty <firepyp650@gmail.com>
2024-09-17 12:16:25 -0500

    Bump version

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ ipbl = DNSBLIpChecker(providers=providers)
 hsbl = DNSBLDomainChecker(providers=providers)
 
 load_dotenv()
-__version__ = "v3.0.21"
+__version__ = "v3.0.22"
 npbase: str = (
     "\[\x0303last\.fm\x03\] [A-Za-z0-9_[\]{}\\|\-^]{1,$MAX} (is listening|last listened) to: \x02.+ - .*\x02( \([0-9]+ plays\)( \[.*\])?)?"  # pyright: ignore [reportInvalidStringEscapeSequence]
 )
