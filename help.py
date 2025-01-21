406ade8d069b244ad677759e3f166d68821b4f09
Firepup Sixfifty <firepyp650@gmail.com>
2025-01-21 14:05:48 -0600

    Early in-progress work on new help system

---
+++ help.py
@@ -0,0 +1,7 @@
+map = {
+    "gen": {"name": "General", "cmds": []},
+    "dbg": {"name": "[DEBUG]", "cmds": []},
+    "adm": {"name": "Admin", "cmds": []},
+    "fun": {"name": "Fun", "cmds": []},
+    "msc": {"name": "[DEBUG]", "cmds": []},
+}
