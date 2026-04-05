332777c33d2ed037539490dfc114e0e3bf273bcd
Firepup Sixfifty <firepup650@firepup650.com>
2026-04-05 11:39:21 -0500

    chore(cleanup): Remove help.py
    I intend to use a different format for help going forwards

--- help.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=missing-module-docstring
-helpMap = {
-    "gen": {"name": "General", "cmds": []},
-    "dbg": {"name": "[DEBUG]", "cmds": []},
-    "adm": {"name": "Admin", "cmds": []},
-    "fun": {"name": "Fun", "cmds": []},
-    "msc": {"name": "[DEBUG]", "cmds": []},
-}
